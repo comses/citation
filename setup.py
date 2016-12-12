c99a5d01bb5246891c2d539320a4a681256599bc
Calvin Pritchard <calvin.pritchard@asu.edu>
2016-12-12 16:20:26 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='comses-citation',
+      version='0.0.1',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+
+          'Intended Audience :: Developers',
+
+          'License :: OSI Approved :: GPL V3 License'
+
+          'Programming Language :: Python 3.5'
+          ],
+
+      description='Citation management and deduplication for Django',
+      url='https://github.com/comses/citation',
+      author='CoMSES',
+      author_email='comses@asu.edu',
+      license='GPL3',
+      packages=['citation'],
+      zip_safe=False
+      )
+
+
