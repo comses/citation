7356a607a5d797e6e341de830e0d5caa66122a8c
Allen Lee <allen.lee@asu.edu>
2017-01-26 17:07:17 -0700

    remove unnecessary deps

--- setup.py
+++ setup.py
@@ -12,16 +12,27 @@ setup(name='citation',
           'License :: OSI Approved :: GPL V3 License'
 
           'Programming Language :: Python 3.5'
-          ],
-
+      ],
       description='Citation management and deduplication for Django',
       url='https://github.com/comses/citation',
       author='CoMSES',
       author_email='comses@asu.edu',
-      install_requires=[req for req in open('requirements.txt').readlines() if req],
+      install_requires=[
+          'bibtexparser>0.6.2',
+          'Django>=1.10,<1.11',
+          'pyzotero',
+          'django-extensions',
+          'django-kronos',
+          'django-model-utils',
+          'djangorestframework>=3.5,<3.6',
+          'fuzzywuzzy>=0.14,<0.15',
+          'lxml>=3.7,<3.8',
+          'psycopg2',
+          'requests',
+          'unidecode',
+          'python-Levenshtein',
+      ],
       license='GPL3',
       packages=['citation'],
       zip_safe=False
       )
-
-
