6e79d04987538d5b8aa9ad2e785c7f66dbd23a46
Dhruvil Patel <dgpatel@outlook.com>
2018-04-30 16:37:23 -0700

    Force creation of publicationmodeldocumentation source code record if code_archived_url is present

--- citation/apps.py
+++ citation/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class CitationConfig(AppConfig):
     name = 'citation'
+
+    def ready(self):
+        """
+        this import it's needed to register the handlers in citation.signals
+        """
+        from . import signals
