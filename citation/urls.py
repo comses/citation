9fa338dd737df8cc062347d6e5dec289b19f65fa
Allen Lee <allen.lee@asu.edu>
2019-04-17 15:10:24 -0700

    add nested routes for optional pub detail slug

--- citation/urls.py
+++ citation/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
@@ -9,6 +8,8 @@ app_name = 'citation'
 # django rest framework endpoints that can generate JSON / HTML
 urlpatterns = format_suffix_patterns([
     path('publications/', views.PublicationList.as_view(), name='publications'),
+    path('publication/<int:pk>/', views.CuratorPublicationDetail.as_view(),
+         name='publication_detail'),
     path('publication/<int:pk>/<slug:slug>/', views.CuratorPublicationDetail.as_view(),
          name='publication_detail'),
     path('notes/', views.NoteList.as_view(), name='notes'),
